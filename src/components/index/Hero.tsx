9596492f58a2b63f01fc5fe456a656796aebd93a
ivangguz <ivangguz@gmail.com>
2024-10-31 21:14:06 -0600

    Actualizacion de archivos

--- src/components/index/Hero.tsx
+++ src/components/index/Hero.tsx
@@ -1,7 +1,15 @@
 export default function Hero() {
     return (    
-    <div className="container">
-        
-    </div>
+        <div className="flex flex-col md:flex-row h-screen">
+            <div className="flex-1 p-4">
+                <h1 className={`text-6xl md:text-8xl font-extrabold mb-6 fira-code-font`}>
+                    Hi my name is Ivan<span className="blinking-cursor">|</span>
+                </h1>
+                <h2 className="text-3xl md:text-5xl font-medium mb-2 text-slate-500">Undergraduate computer science student</h2>
+            </div>
+            <div className="flex-1 p-4">
+                <h1></h1>
+            </div>
+        </div>
     );
 }
